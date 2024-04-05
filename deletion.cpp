4a7cc661ed77469ff5d16935bf176a013ffe0354
DushyantVarshney2005 <165780311+DushyantVarshney2005@users.noreply.github.com>
2024-04-05 16:01:58 +0530

    deletion.cpp
    Function to delete a member of a club

---
+++ deletion.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "node.cpp"
+using namespace std;
+void deletion(node **club[], int id, int num)
+{
+    node **p;
+    if (num <= 10 && num > 0)
+    {
+        p = club[num - 1];
+        node *q;
+        q = p[id % 10];
+        if (q == NULL)
+            cout << "Empty array" << endl;
+        else
+        {
+            if (q->stdid == id)
+            {
+                p[id % 10] = q->next;
+                q->next = NULL;
+                // delete node
+            }
+            else
+            {
+                int c = 0;
+                while (q->next != NULL)
+                {
+                    if (q->stdid == id)
+                    {
+                        c = 1;
+                        node *a;
+                        node *b;
+                        a = q->previous;
+                        b = q->next;
+                        b->previous = a;
+                        a->next = b;
+                        // doubly linked list deletion
+                        break;
+                    }
+                    else
+                        q = q->next;
+                }
+                if (c)
+                    cout << "Wrong id entered" << endl;
+            }
+        }
+    }
+    else
+        cout << "Wrong club number entered" << endl;
+}
