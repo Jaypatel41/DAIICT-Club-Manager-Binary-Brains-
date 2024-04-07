ed95a8bbef7bdf2faf892d86a10f22ba169e1731
PratikChauhan1 <147023732+PratikChauhan1@users.noreply.github.com>
2024-04-08 00:09:33 +0530

    Update print a club.cpp

--- search.cpp
+++ search.cpp
@@ -1,62 +1,13 @@
-void searchbyclub(node **club[], int num)
+void printaclub(const vector<vector<node *>> &Club, list<pair<int, string>> &ls)
 {
-    node **p;
-    p = club[num - 1];
-    node *q;
-    int i = 0;
-    int count = 1;
-    switch (num - 1)
+    string n;
+    cout << "Enter the name of the club " << endl;
+    cin >> n;
+    editString(n);
+    int i = ClubIndex(ls, n);
+    if (i != 100)
     {
-    case 0:
-        cout << "Press Club" << endl;
-        break;
-    case 1:
-        cout << "Debate Club" << endl;
-        break;
-    case 2:
-        cout << "Film Club" << endl;
-        break;
-    case 3:
-        cout << "Dance Club" << endl;
-        break;
-    case 4:
-        cout << "Music Club" << endl;
-        break;
-    case 5:
-        cout << "Garba Club" << endl;
-        break;
-    case 6:
-        cout << "Drama Club" << endl;
-        break;
-    case 7:
-        cout << "Research Club" << endl;
-        break;
-    case 8:
-        cout << "Programming Club" << endl;
-        break;
-    case 9:
-        cout << "Chess Club" << endl;
-        break;
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        q = p[i];
-        while (q != NULL)
-        {
-            cout << count << " . " << q->stdid << " , " << q->name << endl;
-            q = q->next;
-            count++;
-        }
-    }
-}
-
-void printclubi(const vector<vector<node *>> &Club, list<pair<int, string>> &ls, int i)
-{
-    bool clubHasMembers = false;
-    int p = ls.size();
-    string n = ClubName(ls, i);
-    if (i <= p-1)
-    {
+        bool clubHasMembers = false;
         cout << n << "_Club"
              << " Members:" << endl;
         for (int j = 0; j < Club[i].size(); ++j)
