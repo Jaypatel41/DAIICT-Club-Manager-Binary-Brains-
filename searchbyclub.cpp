d1e5a8fca068c74cf927e14f172841715374b0f0
Jaypatel41 <68727212+Jaypatel41@users.noreply.github.com>
2024-04-08 00:03:01 +0530

    Update searchbyclub.cpp

--- searchbyclub.cpp
+++ searchbyclub.cpp
@@ -143,62 +143,42 @@ void searchbyid(node **club[], int id)
         cout << "Member of this ID is currently not present in any of the club of DAIICT" << endl;
 }
 
-void searchbyname(node **club[], string name)
+void searchbyname(vector<vector<node *>> Club, list<pair<int, string>> ls)
 {
-    node **p;
-    int d = 0;
-    for (int i = 0; i < 10; i++)
+    string n;
+    cout<< "Enter the name you want to search" << endl;
+    getline(cin,n);
+    editString(n);
+    cout<<"\n";
+    int k = 0;
+    for (int i = 0; i <Club.size(); i++)
     {
-        p = club[i];
-        node *q;
+        node * p;
+        int count = 0;
         for (int j = 0; j < 10; j++)
         {
-            q = p[j];
-            while (q != NULL)
+            p = Club[i][j];
+            while (p != NULL)
             {
-                if (name == q->name)
+                if (p->name == n)
                 {
-                    d = 1;
-                    cout << "Name - " << q->name << endl;
-                    cout << "Id - " << q->stdid << endl;
-                    switch (i)
-                    {
-                    case 0:
-                        cout << "Press Club" << endl;
-                        break;
-                    case 1:
-                        cout << "Debate Club" << endl;
-                        break;
-                    case 2:
-                        cout << "Film Club" << endl;
-                        break;
-                    case 3:
-                        cout << "Dance Club" << endl;
-                        break;
-                    case 4:
-                        cout << "Music Club" << endl;
-                        break;
-                    case 5:
-                        cout << "Garba Club" << endl;
-                        break;
-                    case 6:
-                        cout << "Drama Club" << endl;
-                        break;
-                    case 7:
-                        cout << "Research Club" << endl;
-                        break;
-                    case 8:
-                        cout << "Programming Club" << endl;
-                        break;
-                    case 9:
-                        cout << "Chess Club" << endl;
-                        break;
-                    }
+                    cout << "Name: " << p->name << endl;
+                    cout << "ID: " << p->stdid << endl; 
+                    count++;
+                    k++;
+                    break;
                 }
-                q = q->next;
+                else 
+                p= p->next;
             }
         }
+        if (count > 0)
+        {
+            cout << ClubName(ls, i) << endl;
+        }
     }
-    if (d == 0)
-        cout << "Member of this name is currently not present in any of the club of DAIICT" << endl;
+    if (k = 0)
+    {
+        cout << "No member found in Any CLUBS\n";
+    }
 }
