7ac38f1f2fbd5462ea0e4381b5b86fe196d04684
Jaypatel41 <68727212+Jaypatel41@users.noreply.github.com>
2024-04-08 00:21:37 +0530

    Update ConvenorFunc.cpp

--- ConvenorFunc.cpp
+++ ConvenorFunc.cpp
@@ -5,3 +5,47 @@ for(int i=0;i<Convenor.size();i++){
     
 }
 }
+void Changeconvenor(vector<vector<string>>& Convenor,vector<int>&Convenori,vector<vector<node*>>Club,list<pair<int,string>>& ls)
+{
+    cout<<"Enter Name of the Club for which you have to change Convenor: ";
+    string clubname;
+    cin >> clubname;
+    editString(clubname);
+    cout<<"\n";
+    for(int i=0;i<10;i++)
+    {
+        if(Convenor[i][0]==clubname)
+        {
+            
+            string name;
+            cout<<"Enter Name of the Convenor: ";
+            getline(cin,name);
+            editString(name);
+            
+            cout<<"\n";
+
+            int id;
+            cout<<"Enter ID of the Convenor: ";
+            cin>>id;
+            
+            int num=ClubIndex(ls,clubname);
+
+            if(num==100)
+            {
+                cout<<"There is NO such Club as "<<clubname<<"_Club.\n";
+                return;
+            }
+            if(memberfound(Club,id,num) && num!=100)
+            {
+                Convenor[i][1] = name;
+                Convenori[i] = id;
+                cout<<"\n\n";
+                cout<<"\t\t\t Convenor is Updated for "<<Convenor[i][0]<<"\n\n";
+                break;
+            }
+            else{cout<<name<<" is not a member of "<<clubname<<"_Club. So we can't assign him as Convenor.\n";return;}            
+        }
+    }
+    cout<<"Updated List of Convenor is given below.\n\n\n";
+    printConvenors(Convenor,Convenori);
+}
