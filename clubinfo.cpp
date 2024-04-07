f823385a15c7504fec8c2332d31be54d0e9a9317
Dhyey01-creator <161374305+Dhyey01-creator@users.noreply.github.com>
2024-04-08 01:20:43 +0530

    Create clubinfo.cpp

---
+++ clubinfo.cpp
@@ -0,0 +1,17 @@
+void Clubinfo(list<pair<int,string>> ls)
+{
+    cout<<"\t\t\t\t\tClubs Currently Running At DA-IICT\n\n";
+    int i=1;
+for (auto it = ls.begin(); it != ls.end(); ++it)
+{
+      if(it->second==" ")
+      {
+          
+      }
+      else
+      {  cout <<i<<") "<< it->second <<"_Club"<< endl;
+          i++;
+      }
+    
+}
+}
